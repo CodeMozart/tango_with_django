890c94b5b3ccf3b310f5d9934760549eb43d8075
zhaomingheaaa <542200245@qq.com>
2017-04-19 12:31:43 +0800

    commit from

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from rango.models import Category,Page
 # Register your models here.
+
+
+class PageInline(admin.StackedInline):
+    model = Page
+    extra = 3
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['name']}),
+        ('Category information', {'fields': ['views', 'likes']})
+    ]
+    inlines = [PageInline]
+
+
+
+
+
+admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Page)
